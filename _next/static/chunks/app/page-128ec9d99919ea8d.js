70edb4de8b8bff655c62ae4ac8e4a504938ae42a
biplab37 <40288287+biplab37@users.noreply.github.com>
2024-04-29 16:10:17 +0000

    Deploying to gh-pages from @ biplab37/FallingWallsNext@f7282645d5060e8b703e92f241e2c62bc8059eae 🚀

---
+++ _next/static/chunks/app/page-128ec9d99919ea8d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,301,327,928],{9398:function(e,n,s){Promise.resolve().then(s.bind(s,1591)),Promise.resolve().then(s.bind(s,4236)),Promise.resolve().then(s.bind(s,4318)),Promise.resolve().then(s.t.bind(s,231,23))}},function(e){e.O(0,[138,874,318,971,23,744],function(){return e(e.s=9398)}),_N_E=e.O()}]);
