6755b1158926caf213ad984dcdcec54360972c8e
biplab37 <biplabmahato37@gmail.com>
2025-04-08 11:31:53 +0200

    changed the title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Header from "./components/header";
 // const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Falling Walls Lab 2025",
+  title: "Falling Walls Lab Wrocław",
   description:
     "The website for Falling walls lab Wrocław 2025. The event will take place on 13th September 2025. The event is organized at University of Wrocław.",
   icons: {
